2e54a383522d4ddfbcbed47f0ced6f0dffd57d07
Reynelle <Rtalford13@gmail.com>
2021-02-25 09:48:52 -0500

    commit tree 8e2729741e09e84ea3d3dcc8a5820b8efefbdbff

--- db/migrate/03_change_datetype_for_birthdate.rb
+++ db/migrate/03_change_datetype_for_birthdate.rb
@@ -1,9 +1,8 @@
-class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+class ChangeDatetypeForBirthdate < ActiveRecord::Migration[5.2]
     
     def change
-            
-        change_column(table_name, column_name, type) :students, :birthdate, :datetime
-         
+        #change_column :table_name, :column_name, :type
+        #change_column(table_name, column_name, type) :students, :birthdate, :datetime 
+        change_column :students, :birthdate, :datetime  
     end 
-
 end
